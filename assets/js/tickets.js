e13e7eb9c03e526f8567c725be70502f3a51eb32
murgantij <javier.murganti@gmail.com>
2023-11-01 22:06:26 -0300

    Agregado de js + add reset +
    Agregado funcion js
    Agregado boton reset
    agregado parrafo total a pagar
    link js en index html
    agregado css clase boton verde   parrafo

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -5,3 +5,25 @@ function obtiene_categoria() {
 function calcula_importe() {
     // calculamos el descuento a partir de la categoria
 }
+
+const btn = document.getElementById('calcularTotal');
+btn.addEventListener('click', calcTotal);
+
+
+function calcTotal() {
+
+    const valorTicket = 200;
+    const cant = document.getElementById('cantidadTickets');
+    const cate = document.getElementById('categoriaTickets');
+    const totalImprimir = document.getElementById('parrafoTotal');
+    
+    
+    let descuento = 1
+    let cantidad = 0
+    let total = 0;
+    cantidad = (cant.value > 0) ? cant.value : 0;    
+    descuento = (cate.value > 0) ? cate.value : 1;
+    total = cantidad * valorTicket * descuento;
+    
+    totalImprimir.innerText = 'Total a Pagar: $ ' + total;
+    }
