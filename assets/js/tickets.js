a2f184c4d67590361d10be0d2a8526cf87ea8eba
murgantij <javier.murganti@gmail.com>
2023-11-02 13:22:29 -0300

    Agregado Validaciones

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -9,9 +9,12 @@ function calcula_importe() {
 const btn = document.getElementById('calcularTotal');
 btn.addEventListener('click', calcTotal);
 
-
 function calcTotal() {
 
+    if (validarFaltanDatos()){
+        alert('Debe Completar los campos del formulario'); 
+        return 'Error';
+    }
     const valorTicket = 200;
     const cant = document.getElementById('cantidadTickets');
     const cate = document.getElementById('categoriaTickets');
@@ -26,4 +29,43 @@ function calcTotal() {
     total = cantidad * valorTicket * descuento;
     
     totalImprimir.innerText = 'Total a Pagar: $ ' + total;
+}
+
+function validarFaltanDatos(){
+    let hayError = false;
+    hayError = validarNombre();
+    hayError = validarApellido();
+    hayError = validarMail();
+    hayError = validarCantidad();
+    return hayError;
+}
+
+function validarCantidad(){
+    const cant = document.getElementById('cantidadTickets');
+    if(cant.value==''){
+        return true;
     }
+}
+
+function validarMail(){
+    const mail = document.getElementById('Email');
+    var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if( validEmail.test(mail.value) ){
+		return true;
+	}else{
+		alert('El Email no es valido');
+		return false;
+	}
+
+    if(mail.value==''){return true;}
+}
+
+function validarNombre(){
+    const nombre = document.getElementById('Nombre');
+    if(nombre.value==''){return true;}
+}
+
+function validarApellido(){
+    const apellido = document.getElementById('Apellido');
+    if(apellido.value==''){return true;}
+}
